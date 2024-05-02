71e1bb085b72c6ebd4b93686269dcd3748979b8e
ron-png <41197190+ron-png@users.noreply.github.com>
2024-05-02 14:47:24 +0200

    RADAU

--- Taschenrechner/Taschenrechner/Form1.Designer.cs
+++ Taschenrechner/Taschenrechner/Form1.Designer.cs
@@ -242,7 +242,7 @@
             this.zahlenFeld.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.zahlenFeld.Location = new System.Drawing.Point(219, 48);
             this.zahlenFeld.Name = "zahlenFeld";
-            this.zahlenFeld.Size = new System.Drawing.Size(320, 20);
+            this.zahlenFeld.Size = new System.Drawing.Size(401, 20);
             this.zahlenFeld.TabIndex = 21;
             this.zahlenFeld.TextChanged += new System.EventHandler(this.zahlenFeld_TextChanged);
             // 
