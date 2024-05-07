a21ea15895f160284d54e23737eeb34dee8a9a5b
ron-png <41197190+ron-png@users.noreply.github.com>
2024-05-07 19:12:51 +0200

    allround
    jetzt die Mathe Berechnungssache

--- Taschenrechner/Taschenrechner/Form1.cs
+++ Taschenrechner/Taschenrechner/Form1.cs
@@ -23,21 +23,24 @@ namespace Taschenrechner
 
         private readonly List<double> numbers = new List<double>(); // Liste mit Zahlen
 
-        bool clearInput;
-
-        int counter = 0; // counter wird gebraucht um zu zählen, ob die letzte Angabe ein rechenoperator war
-        bool kommacounter;
+        bool clearInput; // falls die Textbox gecleart werden muss
+        bool kommacounter; // um zu gucken, ob schon ein Komma eingegeben wurde. Habe vorher "counter"´genutzt, jedoch jetzt bool
 
         public Rechner()
         {
             InitializeComponent();
 
             // damit am Anfang die Null angezeigt wird
-            zahlenFeld.Text = "0";
-            clearInput = true;
+            nullanzeige();
 
         }
 
+        // kleine Funktoin, um die Null anzuzeigen
+        private void nullanzeige()
+        {
+            zahlenFeld.Text = "0";
+            clearInput = true;
+        }
         private void Rechner_Load(object sender, EventArgs e)
         {
 
@@ -61,6 +64,11 @@ namespace Taschenrechner
             {
                 return;
             }
+            else if (clearInput)
+            {
+                zahlenFeld.Text = string.Empty;
+                clearInput = false;
+            }
 
             zahlenFeld.Text += ((Button)sender).Text;
            
@@ -73,13 +81,138 @@ namespace Taschenrechner
 
         private void buttonMal_Click(object sender, EventArgs e)
         {
-            numbers.Add(double.Parse(zahlenFeld.Text));
-            operators.Add(((Button)sender).Text);
-            textBox1.Text = zahlenFeld.Text + ((Button)sender).Text; // fuegt die Zahlen ins obere Feld ein
-            clearInput = true;
-            kommacounter = false;
+            if (zahlenFeld.Text.EndsWith(","))
+            { 
+                zahlenFeld.Text = zahlenFeld.Text.Substring(0, zahlenFeld.Text.Length - 1); // Falls die Eingabe mit einem Komma endet
+            }
+
+            numbers.Add(Convert.ToDouble(zahlenFeld.Text.Replace(',', '.'))); // konvertiert die eingegebene Zahl zu double und fügt diese zu Liste hinzu
+            operators.Add(((Button)sender).Text); // fügt die den Operanten in eine Liste Hinzu
+            textBox1.Text += zahlenFeld.Text + ((Button)sender).Text; // fuegt die Zahlen ins obere Feld ein
+            clearInput = true; // markiert, um das Feld für neuen Input "frei" zu machen
+            kommacounter = false; // Komma kann jetzt auch eingegeben werden
         }
 
+        
+
+
+        private void buttonKomma_Click(object sender, EventArgs e)
+        {
+            if (kommacounter)
+            {
+                return;
+            }
+            else if (clearInput)
+            {
+                zahlenFeld.Text = "0,"; // falls nach einem Operator direkt ein Komma eingegeben wird
+            }
+            else
+            {
+                zahlenFeld.Text += ",";
+            }
+            
+            kommacounter = true;
+            clearInput = false;
+        }
+
+        private void backspace_Click(object sender, EventArgs e)
+        {
+            string formel = zahlenFeld.Text;
+            if (formel.Length > 0)
+            {
+                zahlenFeld.Text = formel.Remove(formel.Length - 1, 1); //https://www.c-sharpcorner.com/blogs/remove-last-character-from-string-in-c-sharp1
+            }
+            else
+            {
+                nullanzeige();
+            }
+        }
+
+        private void c_Click(object sender, EventArgs e)
+        {
+            nullanzeige();
+            operators.Clear();
+            numbers.Clear();
+        }
+
+        private void ce_Click(object sender, EventArgs e)
+        {
+            nullanzeige();
+        }
+
+        
+
+        // Zahlen negieren kek
+        private void vorZeichen_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void buttonGleich_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        #region MATHE 
+        // Berechne das zeug
+        private double Calculate(double number1, double number2, string mathOperator)
+        {
+
+            if (string.IsNullOrEmpty(mathOperator))
+                throw new ArithmeticException("Kein Operator übergeben");
+
+            double result;
+
+            // Switch um entsprechende Rechnung auszuführen
+            switch (mathOperator)
+            {
+                case "+":
+                    result = number1 + number2;
+                    break;
+
+                case "-":
+                    result = number1 - number2;
+                    break;
+
+                case "*":
+                    result = number1 * number2;
+                    break;
+
+                case "/":
+                    if (number2 == 0)
+                    {
+                        throw new ArithmeticException("Teilen durch 0 nicht möglich");
+                    }
+                    else
+                    {
+                        result = number1 / number2;
+                    }
+                    break;
+
+                default:
+                    throw new ArithmeticException(mathOperator + " ist kein gültiger Operator");
+            }
+            return result;
+        }
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #region ungenutzes Zeug
         private void buttonPlus_Click(object sender, EventArgs e)
         {
 
@@ -95,42 +228,6 @@ namespace Taschenrechner
 
         }
 
-        private void buttonGleich_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void buttonKomma_Click(object sender, EventArgs e)
-        {
-            if (kommacounter)
-            {
-                return;
-            }
-            this.zahlenFeld.Text += ",";
-            kommacounter = true;
-            clearInput = false;
-        }
-
-        private void backspace_Click(object sender, EventArgs e)
-        {
-            string formel = zahlenFeld.Text;
-            if (formel.Length > 0)
-            {
-                zahlenFeld.Text = formel.Remove(formel.Length - 1, 1); //https://www.c-sharpcorner.com/blogs/remove-last-character-from-string-in-c-sharp1
-            }            
-        }
-
-        private void c_Click(object sender, EventArgs e)
-        {
-            zahlenFeld.Text = ""; // entfernt den ganzen Textinhalt
-        }
-
-        private void ce_Click(object sender, EventArgs e)
-        {
-            zahlenFeld.Text = "0";
-            clearInput = true;
-        }
-
         private void zahlenFeld_TextChanged(object sender, EventArgs e)
         {
 
@@ -141,11 +238,6 @@ namespace Taschenrechner
 
         }
 
-
-        // Zahlen negieren kek
-        private void vorZeichen_Click(object sender, EventArgs e)
-        {
-            
-        }
+        #endregion
     }
 }
